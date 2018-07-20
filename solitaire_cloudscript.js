475b2661a3067190213cca7d23f81eae8daec5d0
Arik Cohen <arikc@microsoft.com>
2018-07-20 16:21:23 -0700

    First attempt

---
+++ solitaire_cloudscript.js
@@ -0,0 +1,22 @@
+// update statistics for a Solitaire Game
+handlers.processSolitaireGameResult = function (args, context) {
+	
+	var request = {
+		PlayFabId: currentPlayerId, Statistics:
+			[
+				{
+				StatisticName: "SolitaireGamesPlayed",
+				Value: 1
+				},
+				{
+					StatisticName: "SolitaireGamesBestScore",
+					Value: 100
+				},
+				{
+					StatisticName: "SolitaireGamesTotalScore",
+					Value: 100
+				}
+			]
+	};
+	server.UpdatePlayerStatistics(request);
+};
