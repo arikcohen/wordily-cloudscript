5043894dec190e41d680c089fa0dac69e3cc2bed
Arik Cohen <arikc@hotmail.com>
2018-12-03 17:11:53 -0800

    Update sendMessage.js

--- sendMessage.js
+++ sendMessage.js
@@ -1,11 +1,14 @@
-handlers.sendFacebookBotMessage = function (messageTitle=null, messageImage=null, messageSubTitle=null, buttonTitle="Play", payload=null, context_id=null, player_id=null ) {
+handlers.sendFacebookBotMessage = function (args, context ) {
+    // arguements 
+    // messageTitle=null, messageImage=null, messageSubTitle=null, buttonTitle="Play", payload=null, context_id=null, player_id=null
+
     var accessToken = "EAAadeyVj81kBAL4s0dq4z5fzLPu7Flpyi9OPTeLsgGBHp1ZAxbieWuYtPka31lf70wIgxMOyFlsUzQuMka41anYZCpBWzT25ekqCCyhkBAvoBH6dl8GYODBSyNWNFsrxuppCjxUJVrY2Gz9lTBezExV0HDXm7Hi3k7EXYhWgZDZD";
     var uriSendMessage = `https://graph.facebook.com/v2.6/me/messages?access_token=${accessToken}`;
 
     var messageBody = `
     {
         "recipient":{
-          "id":"1983609171705685"
+          "id":"<PSID>"
         },
         "message":{
           "attachment":{
@@ -14,14 +17,14 @@ handlers.sendFacebookBotMessage = function (messageTitle=null, messageImage=null
               "template_type":"generic",
               "elements":[
                  {
-                  "title":"${messageTitle}",
-                  "image_url":"${messageImage}",
-                  "subtitle":"${messageSubTitle}",
+                  "title":"${args.messageTitle}",
+                  "image_url":"${args.messageImage}",
+                  "subtitle":"${args.messageSubTitle}",
                   
                   "buttons":[
                     {
                       "type":"game_play",                      
-                      "title":"${buttonTitle}"
+                      "title":"${args.buttonTitle}"
                     }              
                   ]      
                 }
@@ -30,10 +33,8 @@ handlers.sendFacebookBotMessage = function (messageTitle=null, messageImage=null
           }
         }
     }`;
-
     log.info("body", messageBody);
-    
     var responseString = http.request(uriSendMessage, "post", messageBody, "application/json");
-    
-    log.info("Response to sendMessage: ", responseString);
+    log.info("response from Bot", responseString);
 }
+
