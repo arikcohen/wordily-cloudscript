bfed1eb3f1fc6710c98c813a4b0413d11dd27bf2
Arik Cohen <arikc@hotmail.com>
2018-12-03 17:00:16 -0800

    Create sendMessage.js

---
+++ sendMessage.js
@@ -0,0 +1,35 @@
+handlers.sendFacebookBotMessage = function (messageTitle=null, messageImage=null, messageSubTitle=null, buttonTitle="Play", payload=null, context_id=null, player_id=null ) {
+    var accessToken = "EAAadeyVj81kBAL4s0dq4z5fzLPu7Flpyi9OPTeLsgGBHp1ZAxbieWuYtPka31lf70wIgxMOyFlsUzQuMka41anYZCpBWzT25ekqCCyhkBAvoBH6dl8GYODBSyNWNFsrxuppCjxUJVrY2Gz9lTBezExV0HDXm7Hi3k7EXYhWgZDZD";
+    var uriSendMessage = `https://graph.facebook.com/v2.6/me/messages?access_token=${accssToken}`;
+
+    var messageBody = `
+    {
+        "recipient":{
+          "id":"1983609171705685"
+        },
+        "message":{
+          "attachment":{
+            "type":"template",
+            "payload":{
+              "template_type":"generic",
+              "elements":[
+                 {
+                  "title":"${messageTitle}",
+                  "image_url":"${messageImage}",
+                  "subtitle":"${messageSubTitle}",
+                  
+                  "buttons":[
+                    {
+                      "type":"game_play",                      
+                      "title":"${buttonTitle}"
+                    }              
+                  ]      
+                }
+              ]
+            }
+          }
+        }
+    }`;
+
+    var responseString = http.request(uriSendMessage, "post", messageBody, "application/json");
+}
