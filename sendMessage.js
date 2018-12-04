156b03a82c99ee77a9b22d1e3f43950da3cd2829
Arik Cohen <arikc@hotmail.com>
2018-12-03 17:03:45 -0800

    Update sendMessage.js
    Fixed spelling error

--- sendMessage.js
+++ sendMessage.js
@@ -1,6 +1,6 @@
 handlers.sendFacebookBotMessage = function (messageTitle=null, messageImage=null, messageSubTitle=null, buttonTitle="Play", payload=null, context_id=null, player_id=null ) {
     var accessToken = "EAAadeyVj81kBAL4s0dq4z5fzLPu7Flpyi9OPTeLsgGBHp1ZAxbieWuYtPka31lf70wIgxMOyFlsUzQuMka41anYZCpBWzT25ekqCCyhkBAvoBH6dl8GYODBSyNWNFsrxuppCjxUJVrY2Gz9lTBezExV0HDXm7Hi3k7EXYhWgZDZD";
-    var uriSendMessage = `https://graph.facebook.com/v2.6/me/messages?access_token=${accssToken}`;
+    var uriSendMessage = `https://graph.facebook.com/v2.6/me/messages?access_token=${accessToken}`;
 
     var messageBody = `
     {
