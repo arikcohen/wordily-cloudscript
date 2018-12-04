a3470d9994d2fbf537eb65f208c2cdb39bc4598d
Arik Cohen <arikc@microsoft.com>
2018-12-03 23:25:32 -0800

    fixed Tags.includes

--- sendMessage.js
+++ sendMessage.js
@@ -42,7 +42,7 @@ handlers.sendFacebookBotMessage = function (args, context ) {
     log.info("context", context);
     var uriSendMessage = `https://graph.facebook.com/v2.6/me/messages?access_token=${accessToken}`;
     // check to see if the player has been conencted for bot messages
-    if (!context.playerProfile.Tags.contains('BotSubscribed')) {
+    if (!context.playerProfile.Tags.includes('BotSubscribed')) {
       return { 
         error: {
          "message" : `Player ${currentPlayerId} has not been subscribed for bot messages`
